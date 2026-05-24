b0b619190c659d9ccd5431533ee74602dc56e439
AquilaXk <illusiveman7@gmail.com>
2026-05-24 23:19:30 +0900

    build(config): 모바일 딥링크 실행 설정 검증

--- front/test/features/auth/deeplink/platform_deeplink_config_test.dart
+++ front/test/features/auth/deeplink/platform_deeplink_config_test.dart
@@ -4,12 +4,21 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Android manifest registers the external login callback scheme', () {
-    final manifest = File('android/app/src/main/AndroidManifest.xml').readAsStringSync();
+    final manifest = File(
+      'android/app/src/main/AndroidManifest.xml',
+    ).readAsStringSync();
 
     expect(manifest, contains('flutter_deeplinking_enabled'));
+    expect(manifest, contains('android.permission.INTERNET'));
+    expect(manifest, contains('android.permission.READ_MEDIA_IMAGES'));
+    expect(manifest, contains('android.permission.READ_EXTERNAL_STORAGE'));
+    expect(manifest, contains('android:usesCleartextTraffic="true"'));
     expect(manifest, contains('android:scheme="maumon"'));
     expect(manifest, contains('android:host="auth"'));
     expect(manifest, contains('android:path="/callback"'));
+    expect(manifest, contains('android.intent.action.VIEW'));
+    expect(manifest, contains('android:scheme="https"'));
+    expect(manifest, contains('android:scheme="http"'));
   });
 
   test('iOS Info.plist registers the external login callback scheme', () {
@@ -19,5 +28,10 @@ void main() {
     expect(plist, contains('<false/>'));
     expect(plist, contains('<key>CFBundleURLSchemes</key>'));
     expect(plist, contains('<string>maumon</string>'));
+    expect(plist, contains('<key>LSApplicationQueriesSchemes</key>'));
+    expect(plist, contains('<string>https</string>'));
+    expect(plist, contains('<key>NSAppTransportSecurity</key>'));
+    expect(plist, contains('<key>NSAllowsLocalNetworking</key>'));
+    expect(plist, contains('<key>NSPhotoLibraryUsageDescription</key>'));
   });
 }
